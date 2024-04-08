5ec172dde7b6a26d58abffd5044c72e517e40741
Gde Parama Artha Dharma <gededharma18@gmail.com>
2024-04-08 13:41:07 +0800

    revamp contect bar

--- lib/widget/contact.dart
+++ lib/widget/contact.dart
@@ -2,6 +2,7 @@ import 'package:ecom_web_flutter/style/style.dart';
 import 'package:ecom_web_flutter/utils/separator.dart';
 import 'package:ecom_web_flutter/utils/size.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactBar extends StatefulWidget {
   const ContactBar({Key? key}) : super(key: key);
@@ -25,9 +26,19 @@ class _ContactBarState extends State<ContactBar> {
             size: 16,
           ),
           HorizontalSeparator(width: 1),
-          Text('metronomsound@gmail.com',
-              style: CusTextStyle.bodyText
-                  .copyWith(color: Colors.white, fontWeight: FontWeight.w500)),
+          InkWell(
+            onTap: () {
+              final Uri emailLaunchUri = Uri(
+                scheme: 'mailto',
+                path: 'metronomsound@gmail.com',
+                queryParameters: {'subject': '', 'body': ''},
+              );
+              launchUrl(emailLaunchUri);
+            },
+            child: Text('metronomsound@gmail.com',
+                style: CusTextStyle.bodyText.copyWith(
+                    color: Colors.white, fontWeight: FontWeight.w500)),
+          ),
           Spacer(),
           Icon(
             Icons.phone_in_talk_rounded,
@@ -35,9 +46,18 @@ class _ContactBarState extends State<ContactBar> {
             size: 16,
           ),
           HorizontalSeparator(width: 1),
-          Text('0819 1888 7333',
-              style: CusTextStyle.bodyText
-                  .copyWith(color: Colors.white, fontWeight: FontWeight.w500)),
+          InkWell(
+            onTap: () {
+              final Uri telLaunchUri = Uri(
+                scheme: 'tel',
+                path: '+6281918887333',
+              );
+              launchUrl(telLaunchUri);
+            },
+            child: Text('0819 1888 7333',
+                style: CusTextStyle.bodyText.copyWith(
+                    color: Colors.white, fontWeight: FontWeight.w500)),
+          ),
         ],
       ),
     );
